254c2a9d473693dc270822c1055e34024677735b
fbenutti <fbenutti@gmail.com>
2023-02-22 23:26:25 -0300

    Class 70 - finishing Reactive Forms!

--- src/app/loan-types/loan-types.component.ts
+++ src/app/loan-types/loan-types.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { map } from 'rxjs';
 
 @Component({
@@ -12,6 +12,7 @@ export class LoanTypesComponent implements OnInit {
 
 
   constructor(private fb: FormBuilder) {
+
     //implementation with FormGroup
     // this.addLoanTypeForm = new FormGroup({
     //   'loanName' : new FormControl(),
@@ -25,6 +26,15 @@ export class LoanTypesComponent implements OnInit {
     //   'loanDescription' : new FormControl('This is very good description of a Loan'),
     //   'loanType': new FormControl('Personal Loan')
     // })
+
+    //this is how we define FormArrays - a simple one:
+    let users = new FormArray([
+      new FormControl('Benutti'),
+      new FormControl('Tutorials')
+    ]);
+
+    console.log(users);
+    console.log(users.value);
 
     //another ways of settings values to form
     this.addLoanTypeForm = this.fb.group({
@@ -41,7 +51,20 @@ export class LoanTypesComponent implements OnInit {
           Validators.maxLength(25)
         ])
       ),
+      // 'users': new FormArray([
+      //   new FormControl('Benutti'),
+      //   new FormControl('Tutorials')
+      // ]),
+      'users' : new FormArray([
+        this.fb.group({
+          'name': new FormControl('Felipe'),
+          'age': new FormControl('32'),
+          'dept': new FormControl('fodão'),
+        })
+      ])
     })
+
+
 
 
     // const newLoanObj = {
@@ -66,17 +89,57 @@ export class LoanTypesComponent implements OnInit {
     //this.addLoanTypeForm.patchValue(newLoanObjPatch);
 
 
-
-    this.addLoanTypeForm.valueChanges.subscribe(data => {
-        console.log(this.addLoanTypeForm.valid)
+    this.addLoanTypeForm.statusChanges.subscribe(data => {
         console.log(data)
       }
     )
 
+    this.addLoanTypeForm.valueChanges.subscribe(data => {
+        //console.log(this.addLoanTypeForm.valid)
+        //console.log(data)
+      }
+    )
+
   }
+
+  get users(): FormArray{
+    return this.addLoanTypeForm.get('users') as FormArray;
+  }
+
+  addUser(){
+    let userArray = this.users;
+    let newUser = this.fb.group({
+      'name': '',
+      'age': '',
+      'dept': ''
+    })
+
+    userArray.push(newUser);
+  }
+
+  removerUser(i: any){
+    let userArray = this.users;
+    userArray.removeAt(i);
+  }
+
+  assignDept(i: any){
+    let userArray = this.users;
+
+    if (userArray.value[i].age > 25) {
+      userArray.value[i].dept = "Ministério da Magia";
+    } else {
+      userArray.value[i].dept = "DPTO de Mistérios";
+    }
+    this.users.setValue(userArray.value)
+  }
+
+  getControls(form: FormGroup, key: string){
+    return (<FormArray>form.controls[key]).controls;
+  }
+
   trackLoanName(): void {
     this.addLoanTypeForm.valueChanges.subscribe(data => {
-      console.log(data)
+      //console.log(data)
     });
   }
 
